353b7de46f10e753b633cee1eb1084683704ccff
sarafat27 <sarafat2769@gmail.com>
2022-03-22 13:17:44 +0600

    local storage completed

--- src/utilities/storage.js
+++ src/utilities/storage.js
@@ -1,14 +1,26 @@
 //use local storage to save data
 const addToDb = id => {
-    const quantity = localStorage.getItem(id)
-    if (quantity) {
-        console.log('already exist')
-        const newQuantity = +quantity + 1;
-        localStorage.setItem(id, newQuantity)
+    let shoppingCart;
+
+    //get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart')
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart)
     }
     else {
-        localStorage.setItem(id, 1)
+        shoppingCart = {};
     }
+
+    //add quantity
+    const quantity = shoppingCart[id]
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity
+    }
+    else {
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 export { addToDb }
